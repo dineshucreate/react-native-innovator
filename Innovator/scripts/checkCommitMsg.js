f77f7154f1c4e535db4e66db457e4bd23dd444e1
ajay <ajaysidhu17@gmail.com>
2021-03-02 17:11:14 +0530

    fix: fixed variable casing and updated checks order for more clarity https://trello.com/c/KHg1K1IE

--- Innovator/scripts/checkCommitMsg.js
+++ Innovator/scripts/checkCommitMsg.js
@@ -6,19 +6,19 @@ const projectRootPath = '../../';
 const absoluteFilePath = path.resolve(__dirname, projectRootPath, file);
 const message = fs.readFileSync(absoluteFilePath, 'utf-8');
 const trelloRegExp = /^chore:\s.+\n(\s*)https:\/\/trello.com\/c\/([0-9a-zA-Z]{8})/;
-const fixcommitRegExp = /^fix:\s.+\n(\s*)https:\/\/trello.com\/c\/([0-9a-zA-Z]{8})/;
-const featcommitRegExp = /^feat:\s.+\n(\s*)https:\/\/trello.com\/c\/([0-9a-zA-Z]{8})/;
+const fixCommitRegExp = /^fix:\s.+\n(\s*)https:\/\/trello.com\/c\/([0-9a-zA-Z]{8})/;
+const featCommitRegExp = /^feat:\s.+\n(\s*)https:\/\/trello.com\/c\/([0-9a-zA-Z]{8})/;
 const docsCommitRegExp = /^docs:\s.+\n(\s*)https:\/\/trello.com\/c\/([0-9a-zA-Z]{8})/;
 const fixRegExp = /^fixed sha /;
 const preReleaseRegExp = /^chore\(release\): /;
 
 if (
-  !fixRegExp.test(message.toLowerCase()) &&
   !trelloRegExp.test(message) &&
-  !preReleaseRegExp.test(message.toLowerCase()) &&
-  !fixcommitRegExp.test(message.toLowerCase()) &&
-  !featcommitRegExp.test(message.toLowerCase()) &&
-  !docsCommitRegExp.test(message.toLowerCase())
+  !fixCommitRegExp.test(message.toLowerCase()) &&
+  !featCommitRegExp.test(message.toLowerCase()) &&
+  !docsCommitRegExp.test(message.toLowerCase()) &&
+  !fixRegExp.test(message.toLowerCase()) &&
+  !preReleaseRegExp.test(message.toLowerCase())
 ) {
   throw new Error(
     '⭕ Invalid commit message: please use \n Reviewee "fixed sha <sha>" OR \n feat: <newFeature> \n fix: <fixIssue> \n chore: <chore> \n docs: <docs> \n with a link to the corresponding Trello card in a new line after each',
